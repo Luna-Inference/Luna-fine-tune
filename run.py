da66cea463a5a5856edc83c79af197dfdd99df50
tungvunguyennguyen@gmail.com <tungvunguyennguyen@gmail.com>
2025-05-11 11:33:51 -0400

    Switching code

--- run.py
+++ run.py
@@ -1,17 +1,67 @@
-# Only runs once to login to Hugging Face
-# from huggingface_hub import login
-# login()
-
+# 1. data
 from datasets import load_dataset
-from transformers import AutoTokenizer
+import pandas as pd
+dataset = load_dataset("mteb/tweet_sentiment_extraction")
+df = pd.DataFrame(dataset['train'])
 
-dataset = load_dataset("yelp_review_full")
-tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-cased")
 
-def tokenize(examples):
-    return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-dataset = dataset.map(tokenize, batched=True)
+# 2. tokenizer
 
-small_train = dataset["train"].shuffle(seed=42).select(range(1000))
-small_eval = dataset["test"].shuffle(seed=42).select(range(1000))
+from transformers import GPT2Tokenizer
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+def tokenize_function(examples):
+   return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+# Divide datasets into train and test
+small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
+small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
+
+# 4. initialize base model
+from transformers import GPT2ForSequenceClassification
+
+model = GPT2ForSequenceClassification.from_pretrained("gpt2", num_labels=3)
+
+# 5. evaluate
+import evaluate
+
+metric = evaluate.load("accuracy")
+
+def compute_metrics(eval_pred):
+   logits, labels = eval_pred
+   predictions = np.argmax(logits, axis=-1)
+   return metric.compute(predictions=predictions, references=labels)
+
+# 6. fine-tuning using the trainer method
+from transformers import TrainingArguments, Trainer
+
+training_args = TrainingArguments(
+   output_dir="test_trainer",
+   #evaluation_strategy="epoch",
+   per_device_train_batch_size=1,  # Reduce batch size here
+   per_device_eval_batch_size=1,    # Optionally, reduce for evaluation as well
+   gradient_accumulation_steps=4
+   )
+
+
+trainer = Trainer(
+   model=model,
+   args=training_args,
+   train_dataset=small_train_dataset,
+   eval_dataset=small_eval_dataset,
+   compute_metrics=compute_metrics,
+
+)
+
+trainer.train()
+
+# 7. evaluate model
+trainer.evaluate()
+
+# 8. save model
+model.save_pretrained("gpt2-tweet-sentiment")
+
